f5ca65960dc0abbe7deda5f17ca7eb84e311f86f
Miguel Mascareno <memiguel1314@gmail.com>
2024-06-28 13:43:11 -0700

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,11 @@
-import { Button } from "@/components/ui/button";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-<<<<<<< HEAD
   return (
     <div>
       <Button variant="purple">Click Me</Button>
-=======
-  return ( 
-    <div>
-      <Button variant="outline" size="icon">
-        Click Me
-      </Button>
-      <Button className="bg-emerald-500" size="lg">
-        Purp
-      </Button>
->>>>>>> 9781a17c3d793aa6bb19022d4b11a42929f6176c
     </div>
   );
 }
