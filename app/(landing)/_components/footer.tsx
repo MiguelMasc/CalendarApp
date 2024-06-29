895d0e0a1350c51446a47dae3de0c3db22981ae8
Miguel Mascareno <memiguel1314@gmail.com>
2024-06-28 18:12:57 -0700

    fixed the logo thingyy getting overlrapped when md

--- app/(landing)/_components/footer.tsx
+++ app/(landing)/_components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="flex items-center w-full p-6 bg-background z-50">
       <Logo />
       <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
-        <Button variant="ghost" size="sm">Proivacy Policy</Button>
+        <Button variant="ghost" size="sm">Privacy Policy</Button>
         <Button variant="ghost" size="sm">Terms and Conditions</Button>
       </div>
     </div>
--- app/(landing)/_components/logo.tsx
+++ app/(landing)/_components/logo.tsx
@@ -9,7 +9,7 @@ const font = Poppins({
 
 export const Logo  = () => {
   return (
-    <div className="hidden sm:flex items-center gap-x-2">
+    <div className="hidden md:flex items-center gap-x-2">
       <Image
         src="/logo-dark.svg"
         height="40"
